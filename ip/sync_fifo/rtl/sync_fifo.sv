2836ce268337a28c21761d9aeef04e4957f8a7d0
arkrg <jyng.kang@gmail.com>
2026-06-21 14:19:30 +0900

    modify: fixed sync_fifo logic

--- ip/sync_fifo/rtl/sync_fifo.sv
+++ ip/sync_fifo/rtl/sync_fifo.sv
@@ -12,7 +12,7 @@ module sync_fifo #(
     output                  flag_empty,
     output                  flag_full
 );
-  wire [WIDTH_DATA-1:0] addr_write, addr_read;
+  wire [WIDTH_ADDR-1:0] addr_write, addr_read;
   wire enable_write;
 
   sync_fifo_controller #(
@@ -67,33 +67,35 @@ module sync_fifo_controller #(
   end
 
   always_comb begin : ptr_write_logic
-    n_phase_write = c_phase_write;
     n_ptr_write   = c_ptr_write;
-    if (cmd_push) begin
-      if (!flag_full) begin
-        if (c_ptr_write == DEPTH_FIFO - 1) begin
-          n_ptr_write   = 0;
-          n_phase_write = ~c_phase_write;
-        end else n_ptr_write++;
-      end else n_ptr_write = c_ptr_write;
-    end
-  end
-
-  always_comb begin : ptr_read_logic
-    n_phase_read = c_phase_read;
-    n_ptr_read   = c_ptr_read;
-    if (cmd_pop) begin
-      if (!flag_empty) begin
-        if (c_ptr_read == DEPTH_FIFO - 1) begin
-          n_ptr_read   = 0;
-          n_phase_read = ~c_phase_read;
-        end else n_ptr_read++;
+    n_phase_write = c_phase_write;
+    if (cmd_push && !flag_full) begin
+      if (c_ptr_write == DEPTH_FIFO - 1) begin
+        n_ptr_write   = 0;
+        n_phase_write = ~c_phase_write;
+      end else begin
+        n_ptr_write   = c_ptr_write + 1;
+        n_phase_write = c_phase_write;
       end
     end
   end
 
-  assign flag_full = (c_phase_read == c_phase_write) && (c_ptr_read == c_ptr_write);
-  assign flag_empty = (c_phase_read != c_phase_write) && (c_ptr_read == c_ptr_write);
+  always_comb begin : ptr_read_logic
+    n_ptr_read   = c_ptr_read;
+    n_phase_read = c_phase_read;
+    if (cmd_pop && !flag_empty) begin
+      if (c_ptr_read == DEPTH_FIFO - 1) begin
+        n_ptr_read   = 0;
+        n_phase_read = ~c_phase_read;
+      end else begin
+        n_ptr_read   = c_ptr_read + 1;
+        n_phase_read = c_phase_read;
+      end
+    end
+  end
+
+  assign flag_full = (c_phase_read != c_phase_write) && (c_ptr_read == c_ptr_write);
+  assign flag_empty = (c_phase_read == c_phase_write) && (c_ptr_read == c_ptr_write);
   assign enable_write = cmd_push & (~flag_full);
 endmodule
 
@@ -113,7 +115,9 @@ module sync_fifo_buffer #(
   logic [WIDTH_DATA-1:0] buffer[DEPTH_FIFO];
 
   always_ff @(posedge clk) begin
-    if (enable_write) buffer[addr_write] <= data_push;
+    if (enable_write) begin
+      buffer[addr_write] <= data_push;
+    end
   end
   assign data_pop = buffer[addr_read];
 endmodule
